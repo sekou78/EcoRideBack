9252ee2c7a61a5fa851cff2a81dd96eb2b825066
sekou78 <s.cisse78@gmail.com>
2025-05-05 20:02:15 +0200

    Mis en place de AvisFixtures avec faker

---
+++ src/DataFixtures/AvisFixtures.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Avis;
+use App\Entity\Reservation;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AvisFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const AVIS_NB_TUPLES = 5;
+    public const AVIS_REFERENCE = 'profilConducteur';
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 1; $i <= self::AVIS_NB_TUPLES; $i++) {
+            $avis = (new Avis())
+                ->setNote($faker->numberBetween(1, 5))
+                ->setCommentaire($faker->paragraph(3))
+                ->setIsVisible($faker->boolean())
+                ->setReservation(
+                    $this->getReference(
+                        ReservationFixtures::RESERVATION_REFERENCE . $i,
+                        Reservation::class
+                    )
+                )
+                ->setUser(
+                    $this->getReference(
+                        UserFixtures::USER_REFERENCE . $i,
+                        User::class
+                    )
+                )
+                ->setCreatedAt(
+                    \DateTimeImmutable::createFromMutable(
+                        $faker->dateTimeBetween(
+                            '-6 months',
+                            'now'
+                        )
+                    )
+                )
+                ->setUpdatedAt(
+                    \DateTimeImmutable::createFromMutable(
+                        $faker->dateTimeBetween(
+                            '-6 months',
+                            'now'
+                        )
+                    )
+                );
+
+            $manager->persist($avis);
+
+            $this->addReference(
+                self::AVIS_REFERENCE . $i,
+                $avis
+            );
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ReservationFixtures::class,
+            UserFixtures::class,
+        ];
+    }
+}
